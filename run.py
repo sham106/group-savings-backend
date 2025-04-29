113fc9a24d599fca0a59607fdec90238eb2504f4
unknown <shamsikush@gmail.com>
2025-04-29 22:08:43 +0300

    run.py issue

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables
     app.run(debug=True, port=5000)
-run.py
+
 
 # from app import create_app
 
